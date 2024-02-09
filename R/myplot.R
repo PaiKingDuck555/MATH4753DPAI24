95d3f738b42c9da4ad50b0f188f1bbb1d265fceb
PaiKingDuck555 <damodarhpai1947@gmail.com>
2024-02-09 17:47:06 -0600

    This is the myplot commit to the package

---
+++ R/myplot.R
@@ -0,0 +1,12 @@
+#' 'just a random plot function that is specific to lab 4'
+#'
+#' @param x a numeric vector
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#'
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
